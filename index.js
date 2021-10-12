04db93a8fd4291168c8acbf157d9b6e667f496bf
Stanislas Ormières <stan@stormier.ninja>
2021-10-12 17:58:08 +0000

    feat: :sparkles: Connect to PostgreSQL

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const { Client } = require('pg')
-const client = new Client()
-await client.connect()
-const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-console.log(res.rows[0].message) // Hello world!
-await client.end()
+import dotenv from 'dotenv'
+import pg from 'pg'
+
+dotenv.config()
+
+const { Client } = pg
+
+!async function () {
+  const client = new Client({user: 'stan'})
+  await client.connect()
+  const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+  console.log(res.rows[0].message) // Hello world!
+  await client.end()
+}()
